03f6d43b35c848a5244bd9120acef22ae04a2f0c
EduardoGS (304C) <g.eduardo.g@gmail.com>
2017-03-17 19:25:20 -0300

    Implementação das artes do menu, não terminadas

---
+++ Jogo de 1 Semana/Assets/Scripts/buttonAlpha.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonAlpha : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<CanvasGroup>().alpha = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
